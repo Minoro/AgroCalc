bcb270d1a502f4b16b08cf572bf2dc00c6e098df
André Minoro Fusioka <minorofusioka@hotmail.com>
2016-05-07 15:02:17 -0300

    Criação do spinner para selecionar a distância dos bicos

--- app/src/main/java/br/com/minoro/calcpulverizador/MainActivity.java
+++ app/src/main/java/br/com/minoro/calcpulverizador/MainActivity.java
@@ -2,6 +2,8 @@ package br.com.minoro.calcpulverizador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_distancia_bicos);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_distancia_bicos, R.layout.support_simple_spinner_dropdown_item);
+
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
     }
 }
